bddf45a76fdb17f85be039f78443b3fe8363a57c
Abdisalam <abtii789@gmail.com>
2023-10-26 19:12:16 +0300

    seperated database reset code for issue and users

--- reset_user_table_db.py
+++ reset_user_table_db.py
@@ -1,14 +1,10 @@
-# use this code to clear the database after testing the application. This will ensure that the database is empty when you start the application again.
+# use this code to clear the  users table from the database after testing the application. This will ensure that the database is empty when you start the application again.
 
 import sqlite3
 
 # Connect to the database
 connection = sqlite3.connect("users.db")
 cursor = connection.cursor()
-
-# Clear data from the "issues" table
-cursor.execute("DELETE FROM issues")
-connection.commit()
 
 # Clear data from the "users" table
 cursor.execute("DELETE FROM users")
