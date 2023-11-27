d9edc9f5cb6663f4bf7432e89146eb00e4c3071f
eric-spu <bscnrb591520@spu.ac.ke>
2023-11-27 16:17:50 +0300

    dashboard

--- static/dashboard.js
+++ static/dashboard.js
@@ -39,3 +39,94 @@ function scanSource() {
             console.error('Error scanning URL:', error);
         });
 }
+
+
+// Mock data for testing
+const issues = [
+    { title: 'Issue 1', description: 'Description 1', source: 'Source 1' },
+    { title: 'Issue 2', description: 'Description 2', source: 'Source 2' },
+    // Add more issues as needed
+];
+
+// Function to render issues in the table
+function renderIssues() {
+    const issueTableBody = document.getElementById('issue-table-body');
+    issueTableBody.innerHTML = '';
+
+    issues.forEach((issue, index) => {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td>${issue.title}</td>
+            <td>${issue.description}</td>
+            <td>${issue.source}</td>
+            <td><button onclick="deleteIssue(${index})" class="btn btn-danger">Delete</button></td>
+        `;
+        issueTableBody.appendChild(row);
+    });
+}
+
+// Function to submit a new issue
+function submitIssue() {
+    const title = document.getElementById('issue-title').value;
+    const description = document.getElementById('issue-description').value;
+    const source = document.getElementById('issue-source').value;
+
+    if (title && description && source) {
+        const newIssue = { title, description, source };
+        issues.push(newIssue);
+        renderIssues();
+    } else {
+        alert('Please fill in all fields.');
+    }
+}
+
+// Function to delete an issue
+function deleteIssue(index) {
+    issues.splice(index, 1);
+    renderIssues();
+}
+
+// Initial rendering
+document.addEventListener('DOMContentLoaded', function () {
+    renderIssues();
+});
+
+
+// Function to update an existing issue
+function updateIssue(index) {
+    const title = prompt('Update Title:', issues[index].title);
+    const description = prompt('Update Description:', issues[index].description);
+    const source = prompt('Update Source:', issues[index].source);
+
+    if (title !== null && description !== null && source !== null) {
+        issues[index] = { title, description, source };
+        renderIssues();
+    }
+}
+
+// Function to view details of an issue
+function viewDetails(index) {
+    const issue = issues[index];
+    alert(`Title: ${issue.title}\nDescription: ${issue.description}\nSource: ${issue.source}`);
+}
+
+// Update the renderIssues function
+function renderIssues() {
+    const issueTableBody = document.getElementById('issue-table-body');
+    issueTableBody.innerHTML = '';
+
+    issues.forEach((issue, index) => {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td>${issue.title}</td>
+            <td>${issue.description}</td>
+            <td>${issue.source}</td>
+            <td>
+                <button onclick="updateIssue(${index})" class="btn btn-warning">Update</button>
+                <button onclick="viewDetails(${index})" class="btn btn-info">Details</button>
+                <button onclick="deleteIssue(${index})" class="btn btn-danger">Delete</button>
+            </td>
+        `;
+        issueTableBody.appendChild(row);
+    });
+}
